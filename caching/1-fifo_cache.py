0bd40df3bf795e1ecf67b9e5a046fa044bf0c7e2
Axm4xd <axmedquliyev68@gmail.com>
2025-11-11 15:01:27 +0400

    fix

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -2,11 +2,12 @@
 """
     BaseCache module
 """
+
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ FIFOCache defines a FIFO (First In, First Out) algorithm to use cache.
+    """ FIFOCache defines a FIFO (First In, First Out) algorithm for the cache.
     
     To use:
     >>> my_cache = FIFOCache()
@@ -33,7 +34,7 @@ class FIFOCache(BaseCaching):
     >>> print(self.cache_data)
     {F: "COD", B: "World", C: "Holberton", D: "School"}
     """
-
+    
     def __init__(self):
         """ Initialize the FIFO cache """
         super().__init__()
@@ -47,7 +48,7 @@ class FIFOCache(BaseCaching):
                 item: the value associated with the key
         """
         if key is not None and item is not None:
-            # If the key doesn't exist, we check if we need to discard the first element
+            # If the key doesn't exist in cache, we check if we need to discard the first element
             if key not in self.cache_data:
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                     # FIFO - Discard the first added item (first in)
