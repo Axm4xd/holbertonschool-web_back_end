1540719114cbe819a45f99768438bb301e0719c0
Axm4xd <axmedquliyev68@gmail.com>
2025-11-11 15:00:26 +0400

    fix

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -3,57 +3,67 @@
     BaseCache module
 """
 from base_caching import BaseCaching
-class FIFOCache(BaseCaching):
-    """ FIFOCache define a FIFO algorithm to use cache
-      To use:
-      >>> my_cache = FIFOCache()
-      >>> my_cache.print_cache()
-      Current cache:
-      >>> my_cache.put("A", "Hello")
-      >>> my_cache.print_cache()
-      A: Hello
-      >>> print(my_cache.get("A"))
-      Hello
-      Ex:
-      >>> print(self.cache_data)
-      {A: "Hello", B: "World", C: "Holberton", D: "School"}
-      >>> my_cache.put("C", "Street")
-      >>> print(self.cache_data)
-      {A: "Hello", B: "World", C: "Street", D: "School"}
 
-      >>> my_cache.put("F", "COD")
-      DISCARD: A
-      >>> print(self.cache_data)
-      {F: "COD", B: "World", C: "Holberton", D: "School"}
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines a FIFO (First In, First Out) algorithm to use cache.
+    
+    To use:
+    >>> my_cache = FIFOCache()
+    >>> my_cache.print_cache()
+    Current cache:
+    
+    >>> my_cache.put("A", "Hello")
+    >>> my_cache.print_cache()
+    A: Hello
+    
+    >>> print(my_cache.get("A"))
+    Hello
+
+    Example:
+    >>> print(self.cache_data)
+    {A: "Hello", B: "World", C: "Holberton", D: "School"}
+    
+    >>> my_cache.put("C", "Street")
+    >>> print(self.cache_data)
+    {A: "Hello", B: "World", C: "Street", D: "School"}
+
+    >>> my_cache.put("F", "COD")
+    DISCARD: A
+    >>> print(self.cache_data)
+    {F: "COD", B: "World", C: "Holberton", D: "School"}
     """
+
     def __init__(self):
-        """ Initialize """
+        """ Initialize the FIFO cache """
         super().__init__()
+
     def put(self, key, item):
         """
-            Modify cache data
+            Add an item to the cache.
 
             Args:
-                key: key of the cache
-                item: value of the key
+                key: the key of the cache
+                item: the value associated with the key
         """
         if key is not None and item is not None:
-            # If the key doesn't exist in cache, we check if we need to discard the first element
+            # If the key doesn't exist, we check if we need to discard the first element
             if key not in self.cache_data:
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    # FIFO - Discard the first added item
+                    # FIFO - Discard the first added item (first in)
                     keydel = list(self.cache_data.keys())[0]  # Get the first key in the cache
-                    del self.cache_data[keydel]  # Delete it
-                    print("DISCARD: {}".format(keydel))
+                    del self.cache_data[keydel]  # Delete the first element
+                    print(f"DISCARD: {keydel}")
             self.cache_data[key] = item  # Add the new item to the cache
+
     def get(self, key):
         """
-            Get cache data by key
+            Retrieve an item from the cache by key.
 
             Args:
-                key: key of the cache
+                key: the key of the cache
 
             Return:
-                value of the key
+                value associated with the key, or None if the key doesn't exist
         """
         return self.cache_data.get(key)
